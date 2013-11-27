31f0468cfda584c26fb52e971f42619f57b404d9
tpajenka <tpajenka@foo>
2013-11-27 13:19:02 +0100

    fehlende Dateien

--- dist/build/autogen/Paths_hgraph.hs
+++ dist/build/autogen/Paths_hgraph.hs
@@ -1,34 +1,30 @@
 module Paths_hgraph (
     version,
     getBinDir, getLibDir, getDataDir, getLibexecDir,
-    getDataFileName, getSysconfDir
+    getDataFileName
   ) where
 
 import qualified Control.Exception as Exception
 import Data.Version (Version(..))
 import System.Environment (getEnv)
-import Prelude
-
 catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
 catchIO = Exception.catch
 
 
 version :: Version
 version = Version {versionBranch = [0,0,1], versionTags = []}
-bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+bindir, libdir, datadir, libexecdir :: FilePath
 
-bindir     = "/home/sdressel/.cabal/bin"
-libdir     = "/home/sdressel/.cabal/lib/x86_64-linux-ghc-7.6.3/hgraph-0.0.1"
-datadir    = "/home/sdressel/.cabal/share/x86_64-linux-ghc-7.6.3/hgraph-0.0.1"
-libexecdir = "/home/sdressel/.cabal/libexec"
-sysconfdir = "/home/sdressel/.cabal/etc"
+bindir     = "/home/thomas/.cabal/bin"
+libdir     = "/home/thomas/.cabal/lib/hgraph-0.0.1/ghc-7.4.1"
+datadir    = "/home/thomas/.cabal/share/hgraph-0.0.1"
+libexecdir = "/home/thomas/.cabal/libexec"
 
-getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
 getBinDir = catchIO (getEnv "hgraph_bindir") (\_ -> return bindir)
 getLibDir = catchIO (getEnv "hgraph_libdir") (\_ -> return libdir)
 getDataDir = catchIO (getEnv "hgraph_datadir") (\_ -> return datadir)
 getLibexecDir = catchIO (getEnv "hgraph_libexecdir") (\_ -> return libexecdir)
-getSysconfDir = catchIO (getEnv "hgraph_sysconfdir") (\_ -> return sysconfdir)
 
 getDataFileName :: FilePath -> IO FilePath
 getDataFileName name = do
--- src/Main.hs
+++ src/Main.hs
@@ -17,6 +17,8 @@ module Main (
     main
 ) where
 
+import DCB
+
 import Control.Monad (unless)
 import Control.Parallel.Strategies
 import Control.DeepSeq
@@ -32,51 +34,6 @@ import Data.Either (lefts, rights)
 import Debug.Trace
 import qualified Data.Text as T
 import Data.Text.Encoding
---import Stream hiding (map) --same as Data.Stream imported above?
-import Data.Array.Accelerate hiding (not,(++))
--- change to Data.Array.Accelerate.CUDA as I and link accelerate-cuda to use GPU instead of CPU
--- depends on accelerate-cuda package in cabal, which needs the installed CUDA-stuff form
--- nVidia (nvcc, header-files, ...) and the propriatary driver
-import Data.Array.Accelerate.Interpreter as I
-type Matrix e = Array DIM2 e
-
-type Attr  = Matrix Double
--- Adjecency-Matrix
-type Adj   = Matrix Int8
--- Vector of the Adjecency-Matrix
-type AdjV  = Vector Int8
-newtype Constraints = Matrix Double
--- Graph consists of a Vector denoting which colums of the matrix represents wich originating
--- column in the global adjencency-matrix, the reduces adjencency-matrix of the graph, a
--- matrix of constraints and a scalar denoting the density
-type Density = Scalar Float
-
--- Graph
-type Graph = (Vector Int8, Adj, Constraints, Density)
-
--- Vector of Graphs
-type MultiGraph e = (Vector Int8, Array DIM3 e, Constraints, Density)
-
-
-
-expand :: Acc (MultiGraph Int8)-> Acc Adj -> Acc Attr -> Acc (MultiGraph Int8)
-expand g a att = undefined
-
--- constraint gets a Graph and an Attribute-Matrix and yields true, if the Graph still fulfills
--- all constraints defined via the Attribute-Matrix.
-constraint :: Acc Graph -> Acc Attr -> Acc (Scalar Bool)
-constraint g a = undefined
-
-
--- addPoint gets a graph and a tuple of an adjecancy-Vector with an int wich column of the
--- Adjacency-Matrix the Vector should represent to generate further Graphs
-addPoint :: Acc Graph -> Acc (Adj, (Scalar Int)) -> Acc (MultiGraph Int8)
-addPoint g a = undefined
-
-
--- addablePoints yields all valid addititonsto a Graph
-addablePoints :: Acc Adj -> Acc Graph-> Acc (Vector Int8)
-addablePoints a g = undefined
 
 
 -- TODO: Give createGraph a presized Array and no dynamic [Int].
