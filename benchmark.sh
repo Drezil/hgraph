9e6d0b378ae5f9746e0f48c06a06d80b8ab90d98
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2014-04-18 10:30:56 +0200

    benchmark now remembers results..

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,7 @@ rm stats2
 for j in {1..10}; do 
 	echo "run ${j}..."; 
 	for i in {1..4}; do 
-		./hgraph -t bmark1haskell.N4000E80000adj bmark1haskell.N4000E80000adj.atr sampledata.p +RTS -N`echo "$i"` > graphs 2> stats2 && tail -n 2 graphs >> stats2;
+		./hgraph -t bmark1haskell.N4000E80000adj bmark1haskell.N4000E80000adj.atr sampledata.p +RTS -N`echo "$i"` > graphs 2>> stats2 && tail -n 2 graphs >> stats2;
 		echo -e "\n\n" >> stats2; 
 	done; 
 	echo -e "run ${j}:\n" >> stats2; 
